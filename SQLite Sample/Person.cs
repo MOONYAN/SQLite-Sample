bdb0eda1bb1190d1766f0e9502b5cb8911485178
Owen <1787hz@gmail.com>
2017-05-05 20:12:58 +0800

    create db create table populate person retrieve persons

---
+++ SQLite Sample/Person.cs
@@ -0,0 +1,39 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite_Sample
+{
+    public class Person
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the day of birth.
+        /// </summary>
+        public DateTime DayOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the picture.
+        /// </summary>
+        public byte[] Picture { get; set; }
+    }
+}
---
+++ SQLite Sample/Repository.cs
@@ -0,0 +1,55 @@
+﻿using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SQLite_Sample
+{
+    public class Repository
+    {
+        private static SQLiteConnection DbConnection
+        {
+            get
+            {
+                return new SQLiteConnection(new SQLitePlatformWinRT(), Path.Combine(ApplicationData.Current.LocalFolder.Path, "Storage.sqlite"));
+            }
+        }
+
+        // Create the table if it does not exist 
+        public void CreateTable()
+        {
+            using (var db = DbConnection)
+            {
+                var c = db.CreateTable<Person>();
+                var info = db.GetMapping(typeof(Person));
+            }
+        }
+
+        // Populate the database
+        public void PopulateDB()
+        {
+            using (var db = DbConnection)
+            {
+                Person person = new Person();
+                person.Id = 1;
+                person.Name = "Sean Connery";
+                // (more property assignments here) 
+                var i = db.InsertOrReplace(person);
+            }
+        }
+
+        // Retrieve all Persons
+        public void RetrievePersons()
+        {
+            using (var db = DbConnection)
+            {
+                List<Person> people = (from p in db.Table<Person>() select p).ToList();
+            }
+        }
+    }
+}
